219562bb5e014dbf2600213c31ab61994d12ba9b
Matt Makins <mattmakins@Matt-Makinss-iMac.local>
2014-05-26 08:10:46 +0100

    configures travis ci

--- src/Orderly/Queue/QueueInterface.php
+++ src/Orderly/Queue/QueueInterface.php
@@ -1,12 +1,6 @@
 <?php
 namespace Orderly;
 
-/**
- * Queue class responsible for manageing the queue.
- * 
- * This class is queue framework agnostic and expects to have access
- * to a configured connection factory.
- */
 interface QueueInterface
 {
 }
